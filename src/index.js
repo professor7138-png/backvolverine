59b0664927fa0679b3dfbd925ceb8c699cc5d1c1
usmanmahi773-gif <usmanmahi773@gmail.com>
2026-03-18 02:55:14 +0500

    a link

--- src/index.js
+++ src/index.js
@@ -67,6 +67,7 @@ const io = new Server(server, {
       'http://wolverine-house.netlify.app',
       "https://wolverinehoouse.vercel.app",
       "https://backvolverine.vercel.app",
+      "https://wolverineeeeehouse.vercel.app",
       "https://frontwolverine-2y6za0m6v-professors-projects-7d2a17ff.vercel.app",
       "https://frontwolverine-git-main-professors-projects-7d2a17ff.vercel.app"
     ],
